79e175c22e796b280632024b032c72864398f4bd
hua-lab <huainusa@gmail.com>
2019-01-30 13:04:49 -0500

    Update Lab_03_hello.pl

--- Lab_03_hello.pl
+++ Lab_03_hello.pl
@@ -10,11 +10,11 @@
 
  # This restricts all variables in the code should be defined by my when they first appear in your code which can avoid the mixture of different variables with the same name.
 
-	my $hello=”Hello World”; 
+	my $hello="Hello World";
 
 # A string is much of a line and it is marked with a dollar $ sign.  Again, any variable should be defined with my when it appears at the first time in the code.
 
-	print $hello,”\n”;
+	print $hello,"\n";
 
 #  Here, you don’t give my to $hello because it has been defined with my as a sentence string of Hello World.  Note that the sentence is quoted by a double prime symbol.
 
